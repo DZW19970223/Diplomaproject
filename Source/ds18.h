575cc1569b51a2a4624be280058873437dbf5cbc
zhaowen <1786398174@qq.com>
2020-03-03 15:09:07 +0800

    delete ds18.c and ds18.h

--- Source/ds18.h
+++
@@ -1,8 +0,0 @@
-#ifndef __DS18_H__
-#define __DS18_H__
-void GetDS18Value(unsigned char *pTemp,unsigned char *pTemp1);
-extern unsigned char temp;
-extern unsigned char Ds18b20Initial(void);
-extern void Temp_test(void);
-
-#endif
